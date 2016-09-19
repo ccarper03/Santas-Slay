733f5b8a98bca53d89fa693b2a6ab5ca433dc335
Charles Carper <ccarper03@gmail.com>
2016-09-19 16:09:06 -0400

    Create main menu, made the scenes, created UI.

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerScript : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	public float jumpSpeed = 20;
+	public float gravity = 9.8f;
+	CharacterController controller;
+	Vector3 currentMovement;
+
+	void Start () {
+
+		controller = GetComponent<CharacterController> ();
+		Cursor.lockState = CursorLockMode.Locked;
+
+	}
+
+	void Update () {
+
+		float translation = Input.GetAxis ("Vertical") * moveSpeed;
+		float straffe = Input.GetAxis ("Horizontal") * moveSpeed;
+		translation *= Time.deltaTime;
+		straffe *= Time.deltaTime;
+
+		transform.Translate (straffe, 0, translation);
+
+		if (Input.GetKeyDown ("escape"))
+			Cursor.lockState = CursorLockMode.None;
+
+
+		if (controller.isGrounded == false)
+			currentMovement -= new Vector3 (0, gravity * Time.deltaTime, 0);
+		else
+			currentMovement.y = 0;
+
+			if (controller.isGrounded && Input.GetButtonDown("Jump"))
+					currentMovement.y = jumpSpeed;
+
+		controller.Move (currentMovement * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+    
+    }
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+
+    }
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
