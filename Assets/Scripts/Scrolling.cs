da079109420b5ce8af852050a07500c0a46fec78
Charles Carper <ccarper03@gmail.com>
2016-09-20 23:24:47 -0400

    Added more assests

---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrolling : MonoBehaviour
+{
+    public RectTransform myRect;
+    public Rect rect;
+    public int speed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
