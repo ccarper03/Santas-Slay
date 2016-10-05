ad94965312e61ede3a442a9f4af90fb453815793
Charles Carper <ccarper03@gmail.com>
2016-10-05 06:07:57 -0400

    Animations, fade the scenes.

---
+++ Assets/Scripts/SleighDoorUnlock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SleighDoorUnlock : MonoBehaviour
+{
+    public bool gotKey = false; 
+    public Animation anim;
+
+    //called once at the start of the game
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        if (gotKey)
+        {
+            anim.Play();
+        }
+    }
+}
---
+++ Assets/fadeToMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class fadeToMenu : MonoBehaviour {
+
+    public int delayedTimeAmount = 20;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DelayedTime());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    IEnumerator DelayedTime()
+    {
+        yield return new WaitForSeconds(delayedTimeAmount);
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
